cd437607fd42ef94bd1cf8bffc8175ac831be68a
Dipanshu Kumar <dipanshu@web5lab.net>
2025-04-16 03:49:17 +0000

    persist

--- src/app/store.js
+++ src/app/store.js
@@ -2,14 +2,34 @@ import { configureStore } from "@reduxjs/toolkit";
 import { getLocalStorage } from "../utils/LocalStorage";
 import authReducer from "../../src/features/authSlice";
 import  navigationReducer  from "../features/navigationSlice";
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage
+import { persistStore, persistReducer } from 'redux-persist';
+import { combineReducers } from 'redux';
 
 
-const localData = getLocalStorage();
-
-export const store = configureStore({
-  reducer: {
-    auth: authReducer,
-    navigation: navigationReducer,
-  },
- 
+const rootReducer = combineReducers({
+  auth: authReducer,
+  navigation: navigationReducer,
+  
 });
+
+console.log('rootReducer',rootReducer)
+
+// 2. Create persist config
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['employees', 'admin', 'superAdmin'], // only persist these
+};
+
+// 3. Apply persistReducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// 4. Create store
+export const store = configureStore({
+  reducer: persistedReducer,
+});
+
+// 5. Create persistor
+export const persistor = persistStore(store);
+
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
-import { store } from './app/store.js'
+import { store ,persistor} from './app/store.js'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { PersistGate  } from 'redux-persist/integration/react';
+
 
 const queryClient = new QueryClient();
 
@@ -16,7 +18,10 @@ createRoot(document.getElementById('root')).render(
 
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
+      <PersistGate loading={null} persistor={persistor}>
         <App />
+      </PersistGate>
+
       </QueryClientProvider>
     </Provider>
 
